292eba889efbc42107d23ce64c6c356f39c98859
lianadanae <lianabette@gmail.com>
2020-01-05 11:19:20 -0500

    added score logic

--- client/src/App.js
+++ client/src/App.js
@@ -1,66 +1,70 @@
 import React, {
   Component
 } from 'react';
-import Thumbnail from './components/Thumbnail';
-import Score from './components/Score';
+import cardData from './cards.json';
+import Card from './components/Cards/cards';
+import Wrapper from "./components/Wrapper/wrapper";
+import Header from "./components/Header/header";
+import Score from './components/Score/Score';
+
+
 class App extends Component {
   state = {
-    tiles: [
-      'https://vignette.wikia.nocookie.net/spongebobgalaxy/images/0/07/SpongeBob_SquarePants.png/revision/latest?cb=20171228024014',
-      'https://vignette.wikia.nocookie.net/spongebobgalaxy/images/0/07/SpongeBob_SquarePants.png/revision/latest?cb=20171228024014',
-      'https://vignette.wikia.nocookie.net/spongebobgalaxy/images/0/07/SpongeBob_SquarePants.png/revision/latest?cb=20171228024014'
-    ],
+    cardData,
+    clickedCards: [],
     score: 0,
-    topScore: 0
+    goal: 12
   };
 
-  clickedTiles = [
+  shuffleCardClick = id => {
+    const clickedCards = this.state.clickedCards;
 
-  ];
-
-  //empty clickedTiles array
-  //deploy
-  handleTileClick = (evt) => {
-    
-    console.log(evt);
-    const clickedTile = evt.target.src;
-
-    if (this.clickedTiles.includes(clickedTile)) {
+    if (clickedCards.includes(id)) {
       this.setState({
+        clickedCards: [],
         score: 0
       });
       return;
+    }else{
+      clickedCards.push(id)
+
+      if(clickedCards.length === 12) {
+        this.setState({score: 12, clickedCards: []});
+        console.log('You Win!');
+        return;
     }
+    this.setState({ cardData, clickedCards, score: clickedCards.length});
 
-    const newScore = this.state.score + 1;
-    const topScore = newScore > this.state.topScore ?
-      newScore :
-      this.state.topScore;
-
-    this.clickedTiles.push(clickedTile);
-
-    const shuffled = this.state.tiles.sort(() => 0.5 - Math.random());
-    this.setState({
-      tiles: shuffled,
-      score: newScore,
-      topScore: topScore
-    });
+    for (let i = cardData.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i +1));
+      [cardData[i], cardData[j]] = [cardData[j], cardData[i]];
+    }
+    }
   }
-
+  
   render() {
       return ( 
-        <div className = "App">
-        <Score score={this.state.score} topScore={this.state.topScore}/> 
-        {this.state.tiles.map((tile, idx) => <Thumbnail 
-        src={tile}
-            key = {
-              idx
-            }
-            onClick = {
-              this.handleTileClick
-            }
-            />)
-            } 
+        <div className="App">
+        <Header className="App-header">
+          <div className="jumbotron">
+            <h1 className="display-4">Clicky Game</h1>
+            <p className="lead">Click on an image to earn points, but don't click on any image more than once!</p>
+            <hr className="my-4" />
+          </div>
+        </Header>
+        
+        <Score total={this.state.score} goal={12}/> 
+        <Wrapper>
+        {this.state.cardData.map(card => ( 
+        <Card
+        onClick={this.shuffleCardClick}
+            key={card.id}
+            id={card.id}
+            image={card.image}
+            alt={card.name}
+            />
+            ))} 
+            </Wrapper>
             </div>
           );
         }
